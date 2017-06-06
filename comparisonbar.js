3448d32715ba3fafccbfedd1da553439aaa19129
Keith Chason <keith.chason@gmail.com>
2017-06-06 17:30:15 -0400

    Initial Commit
    Initial Commit

---
+++ comparisonbar.js
@@ -0,0 +1,252 @@
+(function ( $ ) {
+
+	$.fn.comparisonbar = function(options){
+
+		this.each(function() {
+			//Get dimensions of frame
+			var element = $(this);
+			var elementWidth = element.width();
+			var elementHeight = element.height();
+			//Set default options
+			var settings = $.extend(
+			{
+				barHeight: 10,
+				barFillColor: 'rgb(242, 244, 247)',
+				barBorderColor: 'rgb(215, 223, 234)',
+				barBorderWeight: 1,
+				valueFillColor: 'rgb(153, 153, 153)',
+				value: 0,
+				minValue: 0,
+				maxValue: 100,
+				showMinMax: true,
+				minMaxLineColor: 'rgb(215, 223, 234)',
+				minMaxTextColor: 'rgb(215, 223, 234)',
+				markers: {},
+				zones: {},
+				topMarkerValue: null,
+				topMarkerColor: 'purple',
+				bottomMarkerValue: null,
+				bottomMarkerColor: 'purple',
+			}, options, element.data());
+			
+			//Define namespace
+			var namespace = "http://www.w3.org/2000/svg";
+			var svg = document.createElementNS(namespace, "svg");
+			$(svg).attr({
+				width: elementWidth+'px',
+				height: elementHeight+'px',
+				"xmlns:xlink": "http://www.w3.org/1999/xlink",
+			});
+			//Set grid system for building elements
+			svg.setAttribute("viewBox", "0 0 "+elementWidth+" "+elementHeight);
+
+			//EMPTY BAR
+			var bar = document.createElementNS(namespace, "rect");
+			$(bar).attr({
+				x: Math.round(0.05 * elementWidth),
+				y: Math.round((elementHeight-settings.barHeight) / 2),
+				height: settings.barHeight,
+				width: Math.round(0.9 * elementWidth),
+			}).css({
+				'fill': settings.barFillColor,
+				'stroke': settings.barBorderColor,
+				'stroke-width': settings.barBorderWeight
+			});
+			//Add bar to SVG
+			$(svg).append(bar);
+
+			//FILL BAR
+			var barFillPercentage = ((settings.value - settings.minValue) / (settings.maxValue - settings.minValue));
+			//Normalize percentage
+			if(barFillPercentage > 1){
+				barFillPercentage = 1;
+			}
+			if(barFillPercentage < 0){
+				barFillPercentage = 0;
+			}
+			//Create object
+			var barFill = document.createElementNS(namespace, "rect");
+			$(barFill).attr({
+				x: Math.round(0.05 * elementWidth),
+				y: Math.round((elementHeight-settings.barHeight) / 2),
+				height: settings.barHeight,
+				width: Math.round(0.9 * elementWidth * barFillPercentage),
+			}).css({
+				'fill': settings.valueFillColor,
+				'stroke': settings.barBorderColor,
+				'stroke-width': settings.barBorderWeight
+			});
+			//Add bar fill to SVG
+			$(svg).append(barFill);
+
+			//MIN & MAX VALUES
+			if(settings.showMinMax){
+				//Create lines and values
+				var minLine = document.createElementNS(namespace, "line");
+				var maxLine = document.createElementNS(namespace, "line");
+
+				$(minLine).attr({
+					x1: Math.round(0.05 * elementWidth),
+					y1: Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight,
+					x2: Math.round(0.05 * elementWidth),
+					y2: Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight+5
+				}).css({
+					'stroke': settings.minMaxLineColor,
+					'stroke-width': settings.barBorderWeight
+				});
+
+				$(maxLine).attr({
+					x1: Math.round(0.95 * elementWidth),
+					y1: Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight,
+					x2: Math.round(0.95 * elementWidth),
+					y2: Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight+5
+				}).css({
+					'stroke': settings.minMaxLineColor,
+					'stroke-width': settings.barBorderWeight
+				});
+
+				//Add text
+				var minLabel = document.createElementNS(namespace, "text");
+				var maxLabel = document.createElementNS(namespace, "text");
+
+				$(minLabel).attr({
+					x: Math.round(0.05 * elementWidth),
+					y: Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight+17,
+				}).css({
+					'fill': settings.minMaxTextColor,
+				});
+				minLabel.textContent = settings.minValue;
+
+				$(maxLabel).attr({
+					x: Math.round(0.95 * elementWidth)-(settings.maxValue.toString().length * 5),
+					y: Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight+17,
+				}).css({
+					'fill': settings.minMaxTextColor,
+				});
+				maxLabel.textContent = settings.maxValue;
+
+				//Add to SVG
+				$(svg).append(minLine);
+				$(svg).append(maxLine);
+				$(svg).append(minLabel);
+				$(svg).append(maxLabel);
+			}
+
+			//ZONES
+			$.each(settings.zones, function(key, value) {
+				//Merge with defaults
+				var zoneSettings = $.extend(
+				{
+					position: 'top',
+					color: 'rgb(215, 223, 234)',
+					min: 0,
+					max: 0,
+					weight: 3
+				}, value);
+				//Calculate height
+				if(zoneSettings['position'] == 'top'){
+					var zoneY = Math.round((elementHeight-settings.barHeight) / 2);
+				}else{
+					var zoneY = Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight;
+				}
+				
+				//Calculate start and stop
+				var startX = ((zoneSettings['min']-settings.minValue) / (settings.maxValue - settings.minValue));
+				var endX = ((zoneSettings['max'] - settings.minValue) / (settings.maxValue - settings.minValue));
+				//Ensure they're in the range
+				if(startX < 0){ startX = 0; }
+				if(startX > 1){ startX = 1; }
+				if(endX < 0){ endX = 0; }
+				if(endX > 1){ endX = 1; }
+				//Create line
+				var zoneLine = document.createElementNS(namespace, "line");
+
+				$(zoneLine).attr({
+					x1: Math.round(startX * 0.90 * elementWidth)+Math.round(0.05 * elementWidth),
+					y1: zoneY,
+					x2: Math.round(endX * 0.90 * elementWidth)+Math.round(0.05 * elementWidth),
+					y2: zoneY
+				}).css({
+					'stroke': zoneSettings['color'],
+					'stroke-width': zoneSettings['weight']
+				});
+
+				//Add line to SVG
+				$(svg).append(zoneLine);
+
+			});
+
+			//MARKERS
+			$.each(settings.markers, function(key, value) {
+				//Merge with defaults
+				var markerSettings = $.extend(
+				{
+					position: 'top',
+					color: 'rgb(215, 223, 234)',
+					textColor: 'rgb(215, 223, 234)',
+					label: '',
+					value: 0
+				}, value);
+
+				//Create markers
+				//Identify center line
+				var markerPercentage = ((markerSettings['value']) / (settings.maxValue - settings.minValue));
+				var labelX = Math.round((markerPercentage * 0.95 * elementWidth) - (markerSettings['label'].length * 3));
+				var marker = document.createElementNS(namespace, "polygon");
+
+				if(markerSettings['position'] == 'top'){
+					var points = ''.concat(
+						(Math.round(markerPercentage * 0.95 * elementWidth)-10),',',(Math.round((elementHeight-settings.barHeight) / 2)-10),
+						' ',(Math.round(markerPercentage * 0.95 * elementWidth)),',',(Math.round((elementHeight-settings.barHeight) / 2)+5),
+						' ',(Math.round(markerPercentage * 0.95 * elementWidth)+10),',',(Math.round((elementHeight-settings.barHeight) / 2)-10)
+					);
+
+					var labelY = (Math.round((elementHeight-settings.barHeight) / 2)-15);
+
+				}else{
+					var points = ''.concat(
+						(Math.round(markerPercentage * 0.95 * elementWidth)-10),',',(Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight+10),
+						' ',(Math.round(markerPercentage * 0.95 * elementWidth)),',',(Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight-5),
+						' ',(Math.round(markerPercentage * 0.95 * elementWidth)+10),',',(Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight+10)
+					);
+
+					var labelY = (Math.round((elementHeight-settings.barHeight) / 2)+settings.barHeight+25);
+				}
+
+				$(marker).attr({
+					points: points
+				}).css({
+					'fill': markerSettings['color']
+				});
+
+				//Add marker to SVG
+				$(svg).append(marker);
+
+				//Display label if passed
+				if(markerSettings['label'].length > 0){
+					var label = document.createElementNS(namespace, "text");
+
+					$(label).attr({
+						x: labelX,
+						y: labelY,
+					}).css({
+						'fill': markerSettings['textColor'],
+					});
+					label.textContent = markerSettings['label'];
+
+					//Add label
+					$(svg).append(label);
+				}		
+			});
+			
+			//Add fully built SVG to element
+			element.append(svg);
+		});
+
+		return this;
+
+
+
+	};
+
+}( jQuery ));
